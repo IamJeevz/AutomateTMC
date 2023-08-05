10c03c6f079bf52bcb42204303e6c794798cffbd
IamJeevz <jeevansaju1@outlook.com>
2023-08-05 13:09:02 +0530

    remove unwanted Java import

--- src/test/java/page/ATPpage.java
+++ src/test/java/page/ATPpage.java
@@ -1,8 +1,7 @@
 package page;
 
 
-import java.awt.AWTException;
-import java.awt.RenderingHints.Key;
+
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
